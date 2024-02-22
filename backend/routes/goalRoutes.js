464b43d3b1b7fd09d109dbbba767597473c474ee
Me <Simonyegikyan@gmail.com>
2024-02-22 00:30:30 -0800

    more set up, did a bit of front end but yall got those

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,6 +6,18 @@ const Goal = require('../models/Goal');
 // Define your routes here
 
 // Example route
+
+router.post('/', async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const newGoal = new Goal({ title, description });
+    const savedGoal = await newGoal.save();
+    res.json(savedGoal);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 router.get('/', async (req, res) => {
   try {
     const goals = await Goal.find();
@@ -14,5 +26,6 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 
 
+
+
 function App() {
   return (
     <div className="App">
