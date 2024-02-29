c73c0d022da2c1f2c54a34d1eaa1f271eb9dc1e5
faizihaer <faizihaer403@g.ucla.edu>
2024-02-29 02:08:37 -0800

    Implemented sending email function when clicked Nudge button

--- backend/nodeMailer.js
+++ backend/nodeMailer.js
@@ -13,15 +13,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const options = {
-  from: process.env.EMAIL_USERNAME,
-  //HERE SHOULD BE WHERE THE USER's EMAIL SHOULD BE
-  to: "USEREMAIL@email.com",
-  subject: "Welcome to NoBS, where you can stop your BS and get to work",
-  text: "Get Ready to work out",
-};
-
-const sendMail = async () => {
+const sendMail = async (options) => {
   try {
     await transporter.sendMail(options);
     console.log("Email has been sent successfully");
@@ -30,7 +22,44 @@ const sendMail = async () => {
   }
 };
 
-sendMail(transporter, options);
+module.exports = { sendMail };
+
+//----------------------------------------------------------------------------------------------
+//GMAIL ACCOUNT
+
+// const nodemailer = require("nodemailer");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const transporter = nodemailer.createTransport({
+//   service: process.env.EMAIL_SERVICE,
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.EMAIL_USERNAME,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
+
+// const options = {
+//   from: process.env.EMAIL_USERNAME,
+//   //HERE SHOULD BE WHERE THE USER's EMAIL SHOULD BE
+//   to: "USEREMAIL@email.com",
+//   subject: "Welcome to NoBS, where you can stop your BS and get to work",
+//   text: "Get Ready to work out",
+// };
+
+// const sendMail = async () => {
+//   try {
+//     await transporter.sendMail(options);
+//     console.log("Email has been sent successfully");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// sendMail(transporter, options);
 
 //----------------------------------------------------------------------------------------------
 //OUTLOOK EMAIL ACCOUNT
