b0f031397fcba6eaae9094e588262732f7804497
faizihaer <faizihaer403@g.ucla.edu>
2024-03-02 14:58:18 -0800

    fixed merging issue

--- frontend/src/pages/Group.js
+++ frontend/src/pages/Group.js
@@ -7,7 +7,6 @@ const GroupPage = () => {
 
   return (
     <div>
-<<<<<<< HEAD
       <div className="welcome-container">
         <h1>Welcome, {user.name}</h1>
         <div className="profile-circle">
@@ -25,24 +24,6 @@ const GroupPage = () => {
           <button className="create-group-button">Create a Group</button>
         </div>
       </div>
-=======
-        <div className="welcome-container">
-          <h1>Welcome, {user.name}</h1>
-          <div className="profile-circle">
-            <img src={user.picture} alt="User profile" />
-          </div>
-
-          <div className="groupSelection">
-            <h2>Enter a Group ID or Create a New Group to join NoBS!</h2>
-            <div className="input-container">
-              <label htmlFor="groupId">Group ID:</label>
-              <input type="text" id="groupId" placeholder="Enter Group ID" />
-              <button className="submit-button">Submit</button>
-            </div>
-            <button className="create-group-button">Create a Group</button>
-          </div>
-        </div>
->>>>>>> main
     </div>
   );
 };
