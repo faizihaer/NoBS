de79451c3140251affec44365c98666b7ef94412
Via Le <>
2024-03-06 21:33:30 -0800

    CHATBOT WORKS

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,19 +7,27 @@ import Modal from "react-modal";
 import ChatbotModal from "../pages/ChatbotModal";
 
 const modalStyle = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // semi-transparent background
+    zIndex: 9999 // higher z-index than other elements
+  },
   content: {
     position: 'fixed',
     bottom: '20px',
     right: '20px',
     minWidth: '600px',
-    minHeight: '400px',
     maxWidth: '600px',
-    maxHeight: '59vh', // Set maximum height to 80% of the viewport height
-    overflow: 'auto',
+    maxHeight: '65vh', // Set maximum height to 80% of the viewport height
+    overflow: 'hidden',
     borderRadius: '10px',
     boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)',
-    padding: '20px',
-    transform: 'translate(150%, 50%)', // Adjusted to move modal to bottom right corner
+    padding: '0px',
+    transform: 'translate(165%, 43%)', // Adjusted to move modal to bottom right corner
     zIndex: '9999', // high z-index to ensure it's on top of other elements
   },
 };
@@ -111,7 +119,10 @@ const Navbar = () => {
       <Modal
       isOpen={modalIsOpen}
       onRequestClose={closeModal}
-       style={modalStyle}>
+       style={modalStyle}
+       shouldCloseOnOverlayClick={false}>
+        <button onClick={closeModal} className="close-button" 
+        style={{margin:'10px'}}>X</button>
         <ChatbotModal />
         <div style={{minWidth: '400px', minHeight: '300px'}}>
       
