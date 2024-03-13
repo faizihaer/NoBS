63e4ade6bf2cdc6f37a41a07b804336e5e612f7d
willjburns <willjburns80@gmail.com>
2024-03-13 01:08:28 -0700

    Navbar will only show homepage once you've joined a group

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,13 +4,6 @@ const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true },
     name: { type: String, required: true },
-    tasks: [ { type: String } ],
-    taskBools: [ { type: Boolean } ],
-    taskTimes: [ { type: String } ],
-    age: { type: Number },
-    weight: { type: Number },
-    feet: { type: Number },
-    inches: { type: Number },
     group:
       {
         type: mongoose.Schema.Types.ObjectId,
