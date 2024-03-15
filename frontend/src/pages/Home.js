dbf24875fa83c1dfb069e3c5e9f07d17b4297c34
Me <Simonyegikyan@gmail.com>
2024-03-14 19:14:21 -0700

    dashboard changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,37 +5,40 @@ import GroupPlan from "../components/GroupPlan";
 import DailyTasks from "../components/DailyTasks";
 import { useAuth } from "../AuthService";
 import { TasksProvider } from "../TasksContext";
+import Dashboard from '../assets/Dashboard.jpg';
 
 const Home = () => {
   const [ShowPopUpSecond, setShowPopUpSecond] = useState(false);
   const [ShowPopUpHourly, setShowPopUpHourly] = useState(false);
 
-  //const { user, groupId } = useAuth(); once we have group id;
   const { user } = useAuth();
 
   return (
     <TasksProvider>
-      <div className="home">
-        <header className="header">
-          <h1>Welcome, {user.name}!</h1>
-        </header>
+      <div className="background-container">
+        <img src={Dashboard} alt="Dashboard" className="background-image" />
+        <div className="home">
+          <header className="header">
+            <h1>Welcome, {user.name}!</h1>
+          </header>
 
-        <section className="plan">
-          <GroupPlan />
-        </section>
+          <section className="plan">
+            <GroupPlan />
+          </section>
 
-        <section className="daily-tasks">
-          <DailyTasks />
-        </section>
-        <section className="Friend Activity">
-          <FriendActivity
-            user={user}
-            ShowPopUpSecond={ShowPopUpSecond}
-            setShowPopUpSecond={setShowPopUpSecond}
-            ShowPopUpHourly={ShowPopUpHourly}
-            setShowPopUpHourly={setShowPopUpHourly}
-          />
-        </section>
+          <section className="daily-tasks">
+            <DailyTasks />
+          </section>
+          <section className="Friend Activity">
+            <FriendActivity
+              user={user}
+              ShowPopUpSecond={ShowPopUpSecond}
+              setShowPopUpSecond={setShowPopUpSecond}
+              ShowPopUpHourly={ShowPopUpHourly}
+              setShowPopUpHourly={setShowPopUpHourly}
+            />
+          </section>
+        </div>
       </div>
     </TasksProvider>
   );
